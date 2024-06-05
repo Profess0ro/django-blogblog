257afb1d203836effbae12dd6651467e2073917d
Freddy Larsson <professoro88@gmail.com>
2024-06-05 06:54:52 +0000

    add script.js

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-#lr!076#z4u#s#!b*(be320if%4ggg(+*07g47#%%f)+3sladv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-profess0ro-djangoblogbl-gc3yh34d4i8.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
