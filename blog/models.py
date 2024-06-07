24ba249e02da215a195a69c2fb5afe0a9ba24276
Freddy Larsson <professoro88@gmail.com>
2024-06-07 10:57:30 +0000

    add image upload on about page

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,8 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -18,28 +20,24 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
-    
     class Meta:
         ordering = ["-created_on"]
-        
+
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments"
-    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter"
-    )
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
-    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    
+    approved = models.BooleanField(default=False)
+
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
-
